6933b27866c47cd6b320d7a9adba15e89f4d7d2a
Kamil Bobrowski <kamil.bobrowski@gmail.com>
2020-11-02 20:30:19 +0100

    updated translations

--- gcp-translate/index.js
+++ gcp-translate/index.js
@@ -11,10 +11,18 @@ async function quickStart(lang) {
   const dict = {}
   for (const el of list) {
     const [translation] = await translate.translate(el, lang);
-    dict[el] = translation
+    const leadingWsOrig = el.search(/\S/);
+    const trailingWsOrig = el.length - leadingWsOrig - el.trim().length;
+    const leadingWsTrans = translation.search(/\S/);
+    const trailingWsTrans = translation.length - leadingWsTrans - translation.trim().length;
+    const missingLeadingWs = leadingWsOrig - leadingWsTrans;
+    const missingTrailingWs = trailingWsOrig - trailingWsTrans;
+    const addFront = missingLeadingWs > 0 ? missingLeadingWs : 0;
+    const addBack = missingTrailingWs > 0 ? missingTrailingWs : 0;
+    dict[el] = ' '.repeat(addFront) + translation + ' '.repeat(addBack)
   }
   const output = JSON.stringify(dict, undefined, 2);
   fs.writeFileSync(`../${lang}/dict.json`, output);
 }
 
-quickStart('pl');
+quickStart('de');
